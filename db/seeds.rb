656aac93df782d08d2ad5faf75f54b839f00aa09
ColtonCGraham <coltoncgraham@hotmail.com>
2023-06-20 17:42:15 -0500

    Db seeding done

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,32 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
+# AdminUser.create!(email: 'colton@admin.com', password: 'password',
+# password_confirmation: 'password')
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+Customer.destroy_all
 if Rails.env.development?
-  AdminUser.create!(email: 'colton@admin.com', password: 'password',
-                    password_confirmation: 'password')
+
+  5.times do
+    cust = Faker::Name.unique.name
+    @customer = Customer.create!(
+      name: cust,
+      number: Faker::PhoneNumber.cell_phone,
+      email: Faker::Internet.unique.email(name: cust),
+      notes: Faker::Lorem.sentence
+    )
+  end
+
+  5.times do
+    cust = Faker::Name.unique.name
+    @customer = Customer.create!(
+      name: cust,
+      number: Faker::PhoneNumber.cell_phone,
+      notes: Faker::Lorem.sentence
+    )
+  end
 end
